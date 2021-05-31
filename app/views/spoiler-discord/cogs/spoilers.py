22b31eb6c841eb7ab6546517f4a005341f62deac
Jerakin <the_rensvind@hotmail.com>
2021-05-31 20:24:05 +0200

    refactor: simplified sending image

--- app/views/spoiler-discord/cogs/spoilers.py
+++ app/views/spoiler-discord/cogs/spoilers.py
@@ -38,44 +38,33 @@ class ModCog(commands.Cog):
         self.update_cache.cancel()
 
     @commands.command()
-    async def pop(self, ctx: commands.Context):
-        latest = self.spoiler.get_latest()
-        if latest:
-            self.spoiler.delete(latest[0])
-
-    @commands.command()
     async def listen(self, ctx: commands.Context):
         if ctx.channel not in self.conf["channels"]:
             self.conf["channels"].append(ctx.channel.id)
             self.save_servers()
 
+    async def send_image(self, channel, card):
+        image_path, exists = self.spoiler.get_card_image(card)
+        if exists:
+            with image_path.open("rb") as fp:
+                picture = discord.File(fp, filename=card.name)
+            await channel.send(file=picture)
+
     @commands.command()
     async def latest(self, ctx: commands.Context, amount: int = 1):
         for card in self.spoiler.get_latest(amount):
-            image_path, exists = self.spoiler.get_card_image(card)
-            picture = discord.File(image_path)
-            if exists:
-                await ctx.channel.send(file=picture)
+            await self.send_image(ctx.channel, card)
 
     async def spoil(self, card):
         for channel_id in self.conf["channels"]:
-            image_path, exists = self.spoiler.get_card_image(card)
-            if exists:
-                with image_path.open("rb") as fp:
-                    picture = discord.File(fp)
-
-                channel = self.bot.get_channel(channel_id)
-                await channel.send(file=picture)
+            channel = self.bot.get_channel(channel_id)
+            await self.send_image(channel, card)
 
     @commands.command()
     async def force(self, ctx):
         self.spoiler.update_cache()
         for card in self.spoiler.new_cards:
-            image_path, exists = self.spoiler.get_card_image(card)
-            if exists:
-                with image_path.open("rb") as fp:
-                    picture = discord.File(fp)
-                await ctx.channel.send(file=picture)
+            await self.send_image(ctx.channel, card)
 
     @tasks.loop(minutes=5)
     async def update_cache(self):
