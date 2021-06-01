63692f77984384271fbfa671998ce9beaa45c708
Jerakin <the_rensvind@hotmail.com>
2021-06-01 15:19:37 +0200

    fix: removed spoiler cog admin level lock

--- app/views/spoiler-discord/cogs/spoilers.py
+++ app/views/spoiler-discord/cogs/spoilers.py
@@ -5,9 +5,6 @@ class ModCog(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-    async def cog_check(self, ctx):
-        return ctx.message.author.guild_permissions.administrator
-
     @commands.command()
     async def latest(self, ctx: commands.Context, amount: int = 1):
         for card in self.bot.spoiler.get_latest(amount):
